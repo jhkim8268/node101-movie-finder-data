881b8ecf4368cbc533b07b0c0c8d1a3f2119455a
jhkim8268@gmail.com <jhkim8268@gmail.com>
2019-10-08 20:43:41 -0700

    built proxy server and cache

--- server/app.js
+++ server/app.js
@@ -1,8 +1,28 @@
 const express = require('express');
-
+const axios = require('axios');
+const logger = require('morgan');
 
 const app = express();
+app.use(logger('combined'))
 
-// When making calls to the OMDB API make sure to append the '&apikey=8730e0e' parameter
+let cache = {};
+
+app.get('/', (req, res) => {
+  let reqURL = req.url;
+  let siteURL = 'http://www.omdbapi.com' + reqURL + '&apikey=8730e0e';
+  
+  if(!Object.keys(cache).includes(reqURL)) {
+    axios.get(siteURL)
+    .then((response) => {
+      cache[reqURL] = response.data
+      res.send(response.data)
+    })
+    .catch((error) => {
+      res.send(error)
+    })
+  } else {
+    res.send(cache[reqURL])
+  }
+})
 
 module.exports = app;
